8c251176a14c52abcae7757f3cbf311bcd9f538a
Dystian En <dystian.en@kelaspintar.id>
2024-01-18 22:36:53 +0700

    fix: responsive activity

--- src/components/Activity/index.tsx
+++ src/components/Activity/index.tsx
@@ -11,14 +11,14 @@ function Activity() {
       <TitleSection title="Kegiatan" desc="Kegiatan kami di TPQ Asy-Syifa’" />
       <Grid>
         {responseActivity?.data?.map((activity: any, index: number) => (
-          <Grid.Col key={index} span={{ base: 2, md: 4 }}>
+          <Grid.Col key={index} span={{ base: 6, md: 4 }}>
             <Card className="relative overflow-hidden card-activity" p={0}>
               <Image
                 src={activity.file_path}
                 width={200}
                 height={100}
                 alt={activity.file_name}
-                className="w-full h-64 rounded-sm img-activity"
+                className="w-full h-32 md:h-64 rounded-sm img-activity"
               />
               <Box className="absolute h-40 bg-gradient-to-t from-black z-50 w-full bottom-0">
                 <Text c={"white"} className="absolute bottom-3 left-5">
